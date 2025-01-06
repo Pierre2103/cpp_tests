01f8eb792fc38d8aee5d2fab738ae2fd43a14daf
Pierre2103 <pierre.gorin@algosup.com>
2025-01-06 15:03:03 +0100

    Add Google Test and GitHub Actions CI

---
+++ build/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/algosup/Documents/Github/cpp_tests/googletest/googletest
+# Build directory: /home/algosup/Documents/Github/cpp_tests/build/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
